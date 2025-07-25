fdf7bbeafcc664460e3b6478c1747ef9ce093478
Erik Mijwaard <jochem@videotroopers.com>
2025-07-25 23:01:48 +0200

    Deploy version 3.5.3

---
+++ jsx/index.js
@@ -0,0 +1,147 @@
+(function (thisObj) {// ----- EXTENDSCRIPT INCLUDES ------ //"object"!=typeof JSON&&(JSON={}),function(){"use strict";var rx_one=/^[\],:{}\s]*$/,rx_two=/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,rx_three=/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,rx_four=/(?:^|:|,)(?:\s*\[)+/g,rx_escapable=/[\\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,rx_dangerous=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,gap,indent,meta,rep;function f(t){return t<10?"0"+t:t}function this_value(){return this.valueOf()}function quote(t){return rx_escapable.lastIndex=0,rx_escapable.test(t)?'"'+t.replace(rx_escapable,function(t){var e=meta[t];return"string"==typeof e?e:"\\u"+("0000"+t.charCodeAt(0).toString(16)).slice(-4)})+'"':'"'+t+'"'}function str(t,e){var r,n,o,u,f,a=gap,i=e[t];switch(i&&"object"==typeof i&&"function"==typeof i.toJSON&&(i=i.toJSON(t)),"function"==typeof rep&&(i=rep.call(e,t,i)),typeof i){case"string":return quote(i);case"number":return isFinite(i)?String(i):"null";case"boolean":case"null":return String(i);case"object":if(!i)return"null";if(gap+=indent,f=[],"[object Array]"===Object.prototype.toString.apply(i)){for(u=i.length,r=0;r<u;r+=1)f[r]=str(r,i)||"null";return o=0===f.length?"[]":gap?"[\n"+gap+f.join(",\n"+gap)+"\n"+a+"]":"["+f.join(",")+"]",gap=a,o}if(rep&&"object"==typeof rep)for(u=rep.length,r=0;r<u;r+=1)"string"==typeof rep[r]&&(o=str(n=rep[r],i))&&f.push(quote(n)+(gap?": ":":")+o);else for(n in i)Object.prototype.hasOwnProperty.call(i,n)&&(o=str(n,i))&&f.push(quote(n)+(gap?": ":":")+o);return o=0===f.length?"{}":gap?"{\n"+gap+f.join(",\n"+gap)+"\n"+a+"}":"{"+f.join(",")+"}",gap=a,o}}"function"!=typeof Date.prototype.toJSON&&(Date.prototype.toJSON=function(){return isFinite(this.valueOf())?this.getUTCFullYear()+"-"+f(this.getUTCMonth()+1)+"-"+f(this.getUTCDate())+"T"+f(this.getUTCHours())+":"+f(this.getUTCMinutes())+":"+f(this.getUTCSeconds())+"Z":null},Boolean.prototype.toJSON=this_value,Number.prototype.toJSON=this_value,String.prototype.toJSON=this_value),"function"!=typeof JSON.stringify&&(meta={"\b":"\\b","\t":"\\t","\n":"\\n","\f":"\\f","\r":"\\r",'"':'\\"',"\\":"\\\\"},JSON.stringify=function(t,e,r){var n;if(gap="",indent="","number"==typeof r)for(n=0;n<r;n+=1)indent+=" ";else"string"==typeof r&&(indent=r);if(rep=e,e&&"function"!=typeof e&&("object"!=typeof e||"number"!=typeof e.length))throw new Error("JSON.stringify");return str("",{"":t})}),"function"!=typeof JSON.parse&&(JSON.parse=function(text,reviver){var j;function walk(t,e){var r,n,o=t[e];if(o&&"object"==typeof o)for(r in o)Object.prototype.hasOwnProperty.call(o,r)&&(void 0!==(n=walk(o,r))?o[r]=n:delete o[r]);return reviver.call(t,e,o)}if(text=String(text),rx_dangerous.lastIndex=0,rx_dangerous.test(text)&&(text=text.replace(rx_dangerous,function(t){return"\\u"+("0000"+t.charCodeAt(0).toString(16)).slice(-4)})),rx_one.test(text.replace(rx_two,"@").replace(rx_three,"]").replace(rx_four,"")))return j=eval("("+text+")"),"function"==typeof reviver?walk({"":j},""):j;throw new SyntaxError("JSON.parse")})}();// ---------------------------------- //// ----- EXTENDSCRIPT PONYFILLS -----function __objectFreeze(obj) { return obj; }// ---------------------------------- ////import pkg from './package.json' assert { type: 'json' };
+//const { version } = pkg;
+var config = {
+  id: "com.autoextupdate.cep"};
+
+var ns = config.id;
+
+var helloWorld$1 = function helloWorld() {
+  var comp = app.project.activeItem;
+  if (comp && comp instanceof CompItem) {
+    app.beginUndoGroup("Add Hello Layer");
+    var textLayer = comp.layers.addText("Hello from After Effects!");
+    textLayer.name = "martians";
+    app.endUndoGroup();
+    return "✅ Text layer added";
+  } else {
+    return "⚠️ No active comp selected";
+  }
+};
+var createLayer = function createLayer() {
+  var comp = app.project.activeItem;
+  if (comp && comp instanceof CompItem) {
+    app.beginUndoGroup("Add Hello Layer");
+    var textLayer = comp.layers.addText("Another Layer");
+    textLayer.name = "Another";
+    app.endUndoGroup();
+    return "✅ Text layer added";
+  } else {
+    return "⚠️ No active comp selected";
+  }
+};
+var testAlert = function testAlert() {
+  alert("This is a test alert from AEFT!");
+};
+$.writeln("✅ ExtendScript bundle loaded and running");
+
+var aeft = /*#__PURE__*/__objectFreeze({
+  __proto__: null,
+  createLayer: createLayer,
+  helloWorld: helloWorld$1,
+  testAlert: testAlert
+});
+
+var helloVoid = function helloVoid() {
+  alert("test");
+};
+var helloError = function helloError(str) {
+  // Intentional Error for Error Handling Demonstration
+  
+  throw new Error("We're throwing an error");
+};
+var helloStr = function helloStr(str) {
+  alert("ExtendScript received a string: ".concat(str));
+  return str;
+};
+var helloNum = function helloNum(n) {
+  alert("ExtendScript received a number: ".concat(n.toString()));
+  return n;
+};
+var helloArrayStr = function helloArrayStr(arr) {
+  alert("ExtendScript received an array of ".concat(arr.length, " strings: ").concat(arr.toString()));
+  return arr;
+};
+var helloObj = function helloObj(obj) {
+  alert("ExtendScript received an object: ".concat(JSON.stringify(obj)));
+  return {
+    y: obj.height,
+    x: obj.width
+  };
+};
+
+var helloWorld = function helloWorld() {
+  alert("Hello from Illustrator");
+};
+
+var ilst = /*#__PURE__*/__objectFreeze({
+  __proto__: null,
+  helloArrayStr: helloArrayStr,
+  helloError: helloError,
+  helloNum: helloNum,
+  helloObj: helloObj,
+  helloStr: helloStr,
+  helloVoid: helloVoid,
+  helloWorld: helloWorld
+});
+
+var host = typeof $ !== "undefined" ? $ : window;
+
+// A safe way to get the app name since some versions of Adobe Apps broken BridgeTalk in various places (e.g. After Effects 24-25)
+// in that case we have to do various checks per app to deterimine the app name
+
+var getAppNameSafely = function getAppNameSafely() {
+  var compare = function compare(a, b) {
+    return a.toLowerCase().indexOf(b.toLowerCase()) > -1;
+  };
+  var exists = function exists(a) {
+    return typeof a !== "undefined";
+  };
+  var isBridgeTalkWorking = typeof BridgeTalk !== "undefined" && typeof BridgeTalk.appName !== "undefined";
+  if (isBridgeTalkWorking) {
+    return BridgeTalk.appName;
+  } else if (app) {
+    
+    if (exists(app.name)) {
+      
+      var name = app.name;
+      if (compare(name, "photoshop")) return "photoshop";
+      if (compare(name, "illustrator")) return "illustrator";
+      if (compare(name, "audition")) return "audition";
+      if (compare(name, "bridge")) return "bridge";
+      if (compare(name, "indesign")) return "indesign";
+    }
+    
+    if (exists(app.appName)) {
+      
+      var appName = app.appName;
+      if (compare(appName, "after effects")) return "aftereffects";
+      if (compare(appName, "animate")) return "animate";
+    }
+    
+    if (exists(app.path)) {
+      
+      var path = app.path;
+      if (compare(path, "premiere")) return "premierepro";
+    }
+    
+    if (exists(app.getEncoderHost) && exists(AMEFrontendEvent)) {
+      return "ame";
+    }
+  }
+  return "unknown";
+};
+switch (getAppNameSafely()) {
+  case "aftereffects":
+  case "aftereffectsbeta":
+    host[ns] = aeft;
+    break;
+  case "illustrator":
+  case "illustratorbeta":
+    host[ns] = ilst;
+    break;
+}
+// prettier-ignore
+
+// https://extendscript.docsforadobe.dev/interapplication-communication/bridgetalk-class.html?highlight=bridgetalk#appname
+//# sourceMappingURL=index.js.map
+})(this);
